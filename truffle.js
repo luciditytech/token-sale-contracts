73ce951d627c54f5c820d2aaadf7366f3b840e69
Miguel Morales <therevoltingx@gmail.com>
2017-08-02 02:42:04 -0700

    adds production settings

--- truffle.js
+++ truffle.js
@@ -1,9 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: '*' // Match any network id
+    },
+    live: {
+      host: '10.0.0.15',
+      port: 8545,
+      network_id: 1
     }
   }
 };
